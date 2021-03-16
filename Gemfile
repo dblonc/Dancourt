b92efcb3c9561742c1970d4a2fc342b99280a817
Daniel Bloncourt <dblonc@aol.com>
2021-03-15 20:06:50 -0400

    Pre Heroku

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'jquery-rails'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -45,7 +45,7 @@ group :development do
   gem 'pry-rails'
   gem 'annotate'
   gem 'bcrypt'
-  gem 'jquery-rails'
+  
 end
 
 group :test do
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './frontend/index.jsx',
     output: {
-        path: './app/assets/javascripts',
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
     },
     devtool: 'source-map',
@@ -17,7 +17,7 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
-                    query: {
+                    options: {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
